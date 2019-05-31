0eacf499b1487f50f50b9a9d4a05c36dfbef466f
moises <elnaufrago2009@gmail.com>
2019-05-31 17:05:20 -0500

    font awesome

--- src/App.vue
+++ src/App.vue
@@ -16,24 +16,12 @@
 
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
-            <b-nav-form>
-              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-            </b-nav-form>
 
-            <b-nav-item-dropdown text="Lang" right>
-              <b-dropdown-item href="#">EN</b-dropdown-item>
-              <b-dropdown-item href="#">ES</b-dropdown-item>
-              <b-dropdown-item href="#">RU</b-dropdown-item>
-              <b-dropdown-item href="#">FA</b-dropdown-item>
-            </b-nav-item-dropdown>
+            <b-nav-item href="#"><i class="fa fa-user-circle"></i> Login</b-nav-item>
+            <b-nav-item href="#"><i class="fa fa-file-alt"></i> Registro</b-nav-item>
+            <b-nav-item href="#"><i class="fa fa-sign-out-alt"></i> Salir</b-nav-item>
 
-            <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
-              <template slot="button-content"><em>User</em></template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
+
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
