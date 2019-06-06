c2e10a6ee59da5d1f797fb157107f631c8d3bd6a
moises <elnaufrago2009@gmail.com>
2019-06-05 20:48:40 -0500

    opciones documentos

--- src/views/documentos/Documentos.vue
+++ src/views/documentos/Documentos.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <div class="row mt-3">
-      <div class="col-6">
+      <div class="col-lg-6">
         <button class="btn btn-light mr-1">Factura <i class="fa fa-plus-circle"></i></button>
         <button class="btn btn-light">Boleta <i class="fa fa-plus-circle"></i></button>
       </div>
-      <div class="col-6">
-
+      <div class="col-lg-6 ">
+        <div class="form-row">
+          <input type="text" class="form-control offset-md-5 mr-1 col" placeholder="Numero, dni, ruc o Fecha">
+          <button class="btn btn-primary col-2"><i class="fa fa-search"></i> Buscar</button>
+        </div>
       </div>
     </div>
     <h1>Documentos</h1>
