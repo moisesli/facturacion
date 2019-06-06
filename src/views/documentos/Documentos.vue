77da7fc9c583bcdc4ced4529d6558687afd125e6
Moises <elnaufrago2009@gmail.com>
2019-06-06 03:06:37 -0500

    bnase

--- src/views/documentos/Documentos.vue
+++ src/views/documentos/Documentos.vue
@@ -5,8 +5,7 @@
         <button class="btn btn-light mr-1">Factura <i class="fa fa-plus-circle"></i></button>
         <button class="btn btn-light">Boleta <i class="fa fa-plus-circle"></i></button>
       </div>
-      <div class="col-6">
-
+      <div class="col-6">        
       </div>
     </div>
     <h1>Documentos</h1>
